19cd645ee26f563ea8eedcfaa4baa66f5646e1ae
AnnaGolo <golovinaav2019@gmail.com>
2022-03-26 22:54:01 -0700

    Listeners class was added

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -1,4 +1,6 @@
 import Resourses.Base;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pageObjects.LandingPage;
 import pageObjects.LoginPage;
@@ -8,10 +10,20 @@ import java.io.IOException;
 
 public class HomePage extends Base {
 
-    @Test
-    public void basePageNav() throws IOException, InterruptedException {
+    @BeforeTest
+    public void initialize() throws IOException {
         driver = initializeDriver();
         driver.get("https://facebook.com");
+    }
+
+    @AfterTest
+    public void teardown(){
+        driver.close();
+    }
+
+
+    @Test
+    public void basePageNav() throws IOException, InterruptedException {
 
         LoginPage l = new LoginPage(driver);
         l.getEmail().sendKeys("golovinaav2019@gmail.com");
---
+++ src/test/java/Listeners.java
@@ -0,0 +1,66 @@
+import Resourses.Base;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+
+
+public class Listeners extends Base implements ITestListener {
+
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ITestListener.super.onTestStart(result);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ITestListener.super.onTestSuccess(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        WebDriver driver = null;
+        String testMethodName = result.getMethod().getMethodName();
+        try {
+            driver = (WebDriver) result.getTestClass().getRealClass().getDeclaredField("field name").get(result.getInstance());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        try {
+            getScreenShotPath(testMethodName, driver);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ITestListener.super.onTestFailure(result);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        ITestListener.super.onTestFailedWithTimeout(result);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        ITestListener.super.onStart(context);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        ITestListener.super.onFinish(context);
+    }
+}
